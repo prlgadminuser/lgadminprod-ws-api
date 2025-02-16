4c7f5102b1fc5a967d158b4806eb1af8ac9e0374
prlgadminuser <liam.heizmann@gmail.com>
2025-02-16 23:51:57 +0100

    Add files via upload

--- serverlist.js
+++ serverlist.js
@@ -2,10 +2,10 @@
 
     const serverlist = {  // available server adresses - is send with the inventory when the player logs in
         EU: [
-          "s1-eu-sdgame.onrender.com"
+          "wss://s1-eu-sdgame.onrender.com"
         ],
         US: [
-          "s1-us-sdgame.onrender.com",     
+          "wss://s1-us-sdgame.onrender.com",     
         ]
       };
       
