f429b7b5e59fa8883b56adc0f5a99981a14002ba
prlgadminuser <liam.heizmann@gmail.com>
2025-05-28 23:29:58 +0200

    Delete ENV.js

--- ENV.js
+++
@@ -1,7 +0,0 @@
-
-// THIS IS ONLY FOR LOCAL DEV - NEVER ON THE ACTUAL SERVER WHERE ENV IS USED
-const MONGO_URI = "mongodb+srv://sr-server-user:I8u8a8iOBNkunxRK@cluster0.ed4zami.mongodb.net/?retryWrites=true&w=majority"
-const TOKEN_KEY = "d8ce40604d359eeb9f2bff31beca4b4b"
-const DISCORDWEBHOOK = "https://discord.com/api/webhooks/1320922981161107528/KQ_m66iOVDJeXCfSgXX1El9qcsTNC2EKj5d1HZZXiD5pLfPofF5Rb0-QV3MoWgDIaK8_"
-
-module.exports = { MONGO_URI, TOKEN_KEY, DISCORDWEBHOOK }
