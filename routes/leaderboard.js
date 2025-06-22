4a542bf86e81b0cc27174e3bee1468e83953085d
prlgadminuser <liam.heizmann@gmail.com>
2025-06-22 23:16:23 +0200

    Update leaderboard.js

--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -1,7 +1,7 @@
 const { userCollection } = require('./../idbconfig');
 
 const excludedNicknames = ["Liquem", "BotPlayer1", "Cheater42"];
-const limit = 10
+const limit = 50
 const UpdateInterval = 5 * 1000 * 60;
 
 
