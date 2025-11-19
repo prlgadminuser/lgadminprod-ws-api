30f2b00ede5a04c308b8d18546e459e1d52c486b
prlgadminuser <liam.heizmann@gmail.com>
2025-11-19 20:50:39 +0100

    Update leaderboard.js

--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -3,84 +3,99 @@ const LZString = require("lz-string");
 
 const excludedNicknames = ["Liquem", "BotPlayer1", "Cheater42"];
 const limit = 50
-const UpdateInterval = 5 * 1000 * 60;
+const UpdateIntervalMins = 5 // minutes between highscore updates
 
 
 const updateUserDocumentsPlaces = false
 
 const updateHighscores = async () => {
   try {
-
     const highscores = await userCollection
-      .find({}, { projection: { _id: 0, n: "$account.nickname", u: "$account.username", s: "$stats.sp" } })
-      .hint("highscoresIndex")
+      .find(
+        {},
+        {
+          projection: {
+            _id: 0,
+            score: "$stats.sp",
+            username: "$account.username",
+          },
+        }
+      )
+      .hint("highscores_skillpoints")
       .limit(limit)
       .toArray();
 
-
-    if (!highscores || highscores.length === 0) {
+    if (!highscores.length) {
       console.error("No highscores found.");
       return;
     }
 
-
-
     if (updateUserDocumentsPlaces) {
+      const timestamp = Date.now();
+      const bulkOps = highscores.map((player, index) => {
+        const place = index + 1;
+        const placedata = {
+          place: place,
+          time: timestamp,
+        };
 
-    const timestamp = Date.now()
-    const bulkOps = highscores.map((player, index) => {
+        return {
+          updateOne: {
+            filter: { "account.username": player.username },
+            update: { $set: { "stats.place": placedata } },
+            hint: "account.username_1",
+          },
+        };
+      });
 
-      const place = index + 1;
-
-      const placedata = {
-        place: place,
-        time: timestamp,
+      if (bulkOps.length > 0) {
+        const result = await userCollection.bulkWrite(bulkOps);
+      } else {
+        console.log("bulkops highscore error");
       }
-
-      return {
-        updateOne: {
-          filter: { "account.username": player.account.username },
-          update: { $set: { "stats.place": placedata } },
-          hint: "account.username_1"
-        },
-      };
-    });
-
-    if (bulkOps.length > 0) {
-      const result = await userCollection.bulkWrite(bulkOps);
-    } else {
-      console.log("bulkops highscore error")
     }
 
-  }
-
-
-    const usernames = highscores.map(player => player.u);
-
+    const usernames = highscores.map((player) => player.username);
     const playerdetails = await userCollection
-      .find({ "account.username": { $in: usernames } }, { projection: { _id: 0, u: "$account.username", h: "$equipped.hat", c: "$equipped.color", hc: "$equipped.hat_color" } })
-      //.hint("playerProfileIndex")
+      .find(
+        { "account.username": { $in: usernames } },
+        {
+          projection: {
+            _id: 0,
+            username: "$account.username",
+            nickname: "$account.nickname",
+            hat: "$equipped.hat",
+            color: "$equipped.color",
+            hat_color: "$equipped.hat_color",
+          },
+        }
+      )
       .hint("account.username_1")
       .toArray();
 
     const playerDetailsMap = playerdetails.reduce((map, player) => {
-      map[player.u] = { h: player.h, c: player.c, hc: player.hc };
+      map[player.username] = {
+        nickname: player.nickname,
+        hat: player.hat,
+        color: player.color,
+        hat_color: player.hat_color,
+      };
       return map;
     }, {});
 
+    const finalHighscores = highscores.map((player) => {
+      const details = playerDetailsMap[player.username] || {};
 
-    const finalHighscores = highscores.map(player => {
-      const details = playerDetailsMap[player.u] || {};
-
-      return `${player.n}:${player.u}:${player.s}:${details.h}:${details.c}:${details.hc}`;
+      return `${details.nickname}:${player.username}:${player.score}:${details.hat}:${details.color}:${details.hat_color}`;
     });
 
     const highscoresString = JSON.stringify(finalHighscores);
 
     const compressedString = LZString.compress(highscoresString);
 
-    global.highscores = compressedString;
+    console.log(highscoresString);
 
+    global.highscores = compressedString;
   } catch (error) {
     console.error("Error while updating highscores:", error);
   }
@@ -96,7 +111,7 @@ async function setupHighscores() {
   await updateHighscores();
   setInterval(async () => {
     await updateHighscores();
-  }, UpdateInterval);
+  }, UpdateIntervalMins * 60000);
 }
 
 module.exports = {
