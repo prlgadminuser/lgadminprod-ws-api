546114b9f5eb70d50f484f50ff29ea600acdae57
prlgadminuser <liam.heizmann@gmail.com>
2025-12-11 22:15:01 +0100

    Update limitconfig.js

--- limitconfig.js
+++ limitconfig.js
@@ -47,7 +47,7 @@ const ConnectionOptionsRateLimit = {
 };
 
 const apiRateLimiter = new RateLimiterMemory({
-    points: 3,  // 10 requests per second
+    points: 4,  // 10 requests per second
     duration: 1,
 });
 
@@ -93,3 +93,4 @@ module.exports = {
   WS_MSG_SIZE_LIMIT
 
 }
+
