dec8136572ed3aad5af18b13b6ea166ed2ccc046
prlgadminuser <liam.heizmann@gmail.com>
2025-12-19 20:33:57 +0100

    Update award-buyer.js

--- payments/award-buyer.js
+++ payments/award-buyer.js
@@ -1,8 +1,43 @@
 const { userCollection } = require("../idbconfig");
 const OFFERS = require("./offers");
 
+async function awardBuyer(userid, offerid) {
+  const offer = OFFERS[offerid];
+  if (!offer) {
+    return false;
+  }
 
-async function(userid, offerid) {
-const offerData = OFFERS[offerid]
+  try {
+    let result;
 
+    switch (offerid) {
+
+      case "coins_1000":
+        result = await userCollection.updateOne(
+          { "account.username": userid },
+          { $inc: { "currency.coins": 1000 } }
+        );
+        break;
+
+      case "coins_5000":
+        result = await userCollection.updateOne(
+          { "account.username": userid },
+          { $inc: { "currency.coins": 5000 } }
+        );
+        break;
+
+      default:
+        return false;
+    }
+
+    // Ensure update actually succeeded
+    if (!result || result.matchedCount !== 1 || result.modifiedCount !== 1) {
+      return false;
+    }
+
+    return true;
+
+  } catch {
+    return false;
+  }
 }
