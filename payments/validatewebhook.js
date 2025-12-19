d011d96b391b3d15428b0064b66a332cc3d10653
prlgadminuser <liam.heizmann@gmail.com>
2025-12-19 16:36:55 +0100

    Update validatewebhook.js

--- payments/validatewebhook.js
+++ payments/validatewebhook.js
@@ -3,18 +3,12 @@
 const XSOLLA_WEBHOOK_SECRET = process.env.XSOLLA_WEBHOOK_SECRET
 
 
-function validateXsollaSignature(rawBody, signature) {
-  if (!signature) return false;
-
-  const hmac = crypto
-    .createHmac('sha1', XSOLLA_WEBHOOK_SECRET)
-    .update(rawBody)
-    .digest('hex');
-
-  return crypto.timingSafeEqual(
-    Buffer.from(hmac),
-    Buffer.from(signature)
-  );
+function validateXsollaSignature(req) {
+  const signature = req.headers['authorization'];
+  if (!signature || !signature.startsWith('Signature ')) return false;
+const calculated = crypto.createHmac('sha256', XSOLLA_WEBHOOK_SECRET)
+  .update(req.rawBody)
+                          .digest('hex');
+  return signature === `Signature ${calculated}`;
 }
-
 module.exports = validateXsollaSignature
