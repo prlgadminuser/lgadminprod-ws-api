6473970b33037665c3395538a8577c2563cdd2e6
prlgadminuser <liam.heizmann@gmail.com>
2026-01-01 20:57:31 +0100

    Update boxrarityconfig.js

--- boxrarityconfig.js
+++ boxrarityconfig.js
@@ -14,10 +14,19 @@ const rarityConfig = {
         coinsRange: [20, 40],
         itemCount: 2,
         customItems: [
-            "A003", "A004", "A005", "A006", "A007", "A008", "A009", "A010",
-            "A011", "A012", "A013", "A014", "A015", "A016", "A017", "A018",
-            "A019", "A020", "A021", "A022", "A023", "A025", "A026", "A030", 
-            "A031", "A034"
+            'HAT:gang_mask','HAT:hot_angel','HAT:gang_mask_glow',
+                'HAT:factory','HAT:star_grey','HAT:star_purple',
+                'HAT:star_bw','HAT:star_multi','HAT:twists',
+                'HAT:sunny_fire','HAT:hot_cheeto','HAT:fuzzy_heart',
+                'HAT:cyber','HAT:clown','HAT:dirtypixels',
+                'HAT:magic','HAT:glow_hero','HAT:chiller',
+                'HAT:tropical','HAT:dream_guard','HAT:pixel_glasses',
+                'HAT:wizard','HAT:lucky_plucky','HAT:astro',
+                'HAT:diver','HAT:mod_cut','HAT:arcade',
+                'HAT:chef','HAT:robot','HAT:cracker',
+                'HAT:magicstone','HAT:santa','HAT:new_year',
+                'HAT:explorer','HAT:weird_mask','HAT:chrono',
+                'HAT:netjump','HAT:pumpkin'
         ],
     },
 
@@ -26,9 +35,16 @@ const rarityConfig = {
         coinsRange: [25, 50],
         itemCount: 2,
         customItems: [
-            "B001", "B002", "B003", "B004", "B005", "B006", "B007", "B008",
-            "B009", "B010", "B011", "B012", "B013", "B014", "B015", "B016",
-            "B017", "B018", "B019", "B020", "B024", "B025"
+              'TOP:gang','TOP:hot_angel','TOP:factory',
+                'TOP:star_grey','TOP:star_purple','TOP:star_bw',
+                'TOP:star_multi','TOP:basic','TOP:sunny_fire',
+                'TOP:hot_cheeto','TOP:fuzzy_heart','TOP:cyber',
+                'TOP:clown','TOP:pixel_nthing','TOP:magic',
+                'TOP:glow_hero','TOP:chiller','TOP:tropical',
+                'TOP:dream_guard','TOP:wizard','TOP:astro',
+                'TOP:random','TOP:chef','TOP:robot',
+                'TOP:cracker','TOP:magicstone','TOP:santa',
+                'TOP:netjump','TOP:pumpkin'
         ],
     },
 
