cf6ee3440c76be0420b250412deefe72758c848a
prlgadminuser <liam.heizmann@gmail.com>
2026-01-20 14:10:12 +0100

    Update leaderboard.js

--- routes/social/leaderboard.js
+++ routes/social/leaderboard.js
@@ -2,8 +2,8 @@ const { userCollection } = require('../../idbconfig');
 const LZString = require("lz-string");
 const { getUserIdPrefix } = require('../../utils/utils');
 
-const limit = 50
-const UpdateInterval = 60 * 1000 * 60 // minutes between highscore updates
+const limit = 100
+const UpdateInterval = 1 * 1000 * 60 // minutes between highscore updates
 
 
 const updateUserDocumentsPlaces = true
@@ -97,4 +97,5 @@ module.exports = {
   gethighscores,
   UpdateInterval
 };
+
 
