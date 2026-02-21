b226fe6fe9efd0ca4448e3031f1087dd80dbbef4
prlgadminuser <liam.heizmann@gmail.com>
2026-02-21 16:35:08 +0100

    Update getinventory.js

--- routes/main/getinventory.js
+++ routes/main/getinventory.js
@@ -1,184 +1,180 @@
-const { battlePassCollection, userCollection, userItemsCollection, shopcollection, userWeaponsCollection } = require('../../idbconfig');
-const { serverlist, getServerByCountry } = require('../../serverlist');
-const { WeaponsToBuy } = require('./buyWeapon');
-const { loadout_allowed_items } = require('./updateLoadout');
-const { RealMoneyPurchasesEnabled } = require('../../index');
-const { rarityPercentages } = require('../../boxrarityconfig');
-const { OFFERKEYS } = require('../../payments/offers');
-const { getUserIdPrefix } = require('../../utils/utils');
+const {
+  battlePassCollection,
+  userCollection,
+  userItemsCollection,
+  shopcollection,
+  userWeaponsCollection,
+} = require("../../idbconfig");
+const { serverlist, getServerByCountry } = require("../../serverlist");
+const { WeaponsToBuy } = require("./buyWeapon");
+const { loadout_allowed_items } = require("./updateLoadout");
+const { RealMoneyPurchasesEnabled } = require("../../index");
+const { rarityPercentages } = require("../../boxrarityconfig");
+const { OFFERKEYS } = require("../../payments/offers");
+const { getUserIdPrefix } = require("../../utils/utils");
 
-
-
-async function getPlayerItems(username) {
-
-    const itemDocuments = await userItemsCollection.find(
-        { userid: username },
-        { projection: { _id: 0, itemid: 1, time: 1 } } // Project only the itemId field and exclude the _id
+async function getPlayerItems(userId) {
+  const itemDocuments = await userItemsCollection
+    .find(
+      { userid: userId },
+      { projection: { _id: 0, itemid: 1, time: 1 } }, // Project only the itemId field and exclude the _id
     )
+    .sort({ time: 1 })
     .limit(100)
-    .hint("player_item_unique")
+    //.hint("player_item_unique")
     .toArray();
-    
-    //itemDocuments.sort((a, b) => b.time - a.time); //descending
-    itemDocuments.sort((a, b) => a.time - b.time); //ascending
 
-    const itemsArray = itemDocuments.map(doc => doc.itemid);
+  //itemDocuments.sort((a, b) => b.time - a.time); //descending
 
-    return itemsArray
+
+ // itemDocuments.sort((a, b) => a.time - b.time); //ascending // prefered version
+
+  const itemsArray = itemDocuments.map((doc) => doc.itemid);
+
+  return itemsArray;
 }
 
 async function getPlayerWeaponsData(username) {
-    // Find all weapons for the player, excluding _id and uid
-    const itemDocuments = await userWeaponsCollection.find(
-        { uid: username },                 // filter by player
-        { projection: { _id: 0, uid: 0 } } // exclude _id and uid
+  // Find all weapons for the player, excluding _id and uid
+  const itemDocuments = await userWeaponsCollection
+    .find(
+      { uid: username }, // filter by player
+      { projection: { _id: 0, uid: 0 } }, // exclude _id and uid
     )
     .limit(100)
-    .hint("weaponindex")                  // use the compound index
+    .hint("weaponindex") // use the compound index
     .toArray();
 
-    // Convert array into an object keyed by wid
-    const weaponsByWid = {};
-    for (const doc of itemDocuments) {
-        if (doc.wid) {
-            weaponsByWid[doc.wid] = doc;
-        }
+  // Convert array into an object keyed by wid
+  const weaponsByWid = {};
+  for (const doc of itemDocuments) {
+    if (doc.wid) {
+      weaponsByWid[doc.wid] = doc;
     }
+  }
 
-    return weaponsByWid;
+  return weaponsByWid;
 }
-
-
 
 //getPlayerItems("Lique")
 // .then(items => {
- //  console.log("Player items:", items);
+//  console.log("Player items:", items);
 //})
 
-
-
-
-   
-
 async function getUserInventory(userId) {
-    try {
-        // Prepare promises for parallel execution
-        const promises = [
-            await userCollection.findOneAndUpdate(
-                 getUserIdPrefix(userId),
-                { $set: { "account.last_login": Date.now() } },
-                {
-                  //  returnDocument: "after", // Return the document after update
-                    projection: {
-                        "account": 1,
-                        "equipped": 1,
-                        "currency": 1,
-                        "inventory": 1,
-                        "stats.sp": 1,
-                    },
-                  //  hint: "account.username_1"
-                }
-            ),
-            
-             ];
+  try {
+    // Prepare promises for parallel execution
+    const promises = [
+      await userCollection.findOneAndUpdate(
+        getUserIdPrefix(userId),
+        { $set: { "account.last_login": Date.now() } },
+        {
+          //  returnDocument: "after", // Return the document after update
+          projection: {
+            account: 1,
+            equipped: 1,
+            currency: 1,
+            inventory: 1,
+            "stats.sp": 1,
+          },
+          //  hint: "account.username_1"
+        },
+      ),
+    ];
 
-        promises.push(
-            await battlePassCollection.findOne(
-                 getUserIdPrefix(userId),
-                {
-                    projection: {
-                        ss_passtier: 1,
-                        ss_coins: 1,
-                        ss_damage: 1,
-                    }
-                }
-            ).catch(() => null), // Handle battle pass collection errors
-        );
+    promises.push(
+      await battlePassCollection
+        .findOne(getUserIdPrefix(userId), {
+          projection: {
+            ss_passtier: 1,
+            ss_coins: 1,
+            ss_damage: 1,
+          },
+        })
+        .catch(() => null), // Handle battle pass collection errors
+    );
 
-        // Wait for all promises to resolve
-        const [userRow, bpuserRow] = await Promise.all(promises);
+    // Wait for all promises to resolve
+    const [userRow, bpuserRow] = await Promise.all(promises);
 
-        if (!userRow) {
-            throw new Error("User not found");
-        }
-
-        // Get current timestamps
-        const currentTimestampInGMT = new Date().getTime();
-
-
-        const season_passtier = bpuserRow ? bpuserRow.ss_passtier || 0 : 0;
-        const season_coins = bpuserRow ? bpuserRow.ss_coins || 0 : 0;
-        const bonusitem_damage = bpuserRow ? bpuserRow.ss_damage || 0 : 0;
-
-        const userInventory = await getPlayerItems(userId)
-
-        const globalConfig = global.config
-       // const userWeaponData = await getPlayerWeaponsData(username)
-
-        const skillpassdata = {
-         tier: season_passtier,
-         bucks: season_coins,
-         season_damage: bonusitem_damage
-        }
-          
-
-        const inventory = {
-            userId: userRow._id,
-            username: userRow.account.username,
-            lastnameupdate: userRow.account.nameupdate || 0,
-
-            items: userInventory,
-            
-            coins: userRow.currency.coins,
-            boxes: userRow.currency.boxes,
-
-            sp: userRow.stats.sp,
-
-            seasondata: {
-            season_end: globalConfig.season_end,
-            skillpass: skillpassdata
-            },
-
-            globalConfig: globalConfig,
-
-            weapons: userRow.inventory.weapons,
-           // weapondata: userWeaponData,
-            loadout: userRow.equipped.loadout,
-            last_collected: userRow.inventory.daily_reward.last_collected_at || 0,
-            hat: userRow.equipped.hat || 0,
-            top: userRow.equipped.top || 0,
-            banner: userRow.equipped.banner || 0,
-            pose: userRow.equipped.pose || 0,
-            color: userRow.equipped.color || 0,
-            hat_color: userRow.equipped.hat_color,
-            top_color: userRow.equipped.top_color,
-            banner_color: userRow.equipped.banner_color,
-            gadget: userRow.account.gadget || 1,
-
-
-            boxrarities: rarityPercentages,
-            //  friends: userRow.social.friends || [],
-            // requests: userRow.social.requests || [],
-            serverlist,
-            nearestRegion: getServerByCountry(userRow.account.country_code || "Unknown"),
-            weaponcatalog: WeaponsToBuy,
-            loadout_allowed_items: loadout_allowed_items,
-            in_app_purchases: RealMoneyPurchasesEnabled ? OFFERKEYS : "disabled" ,
-
-            server_timestamp: currentTimestampInGMT,
-        };
-  
-        // Return the constructed object
-        return inventory;
-      //  })
-    } catch (error) {
-        // Catch and rethrow errors with additional context
-        throw new Error(`Failed to get user inventory: ${error.message}`);
-        
+    if (!userRow) {
+      throw new Error("User not found");
     }
-   
+
+    // Get current timestamps
+    const currentTimestampInGMT = new Date().getTime();
+
+    const season_passtier = bpuserRow ? bpuserRow.ss_passtier || 0 : 0;
+    const season_coins = bpuserRow ? bpuserRow.ss_coins || 0 : 0;
+    const bonusitem_damage = bpuserRow ? bpuserRow.ss_damage || 0 : 0;
+
+    const userInventory = await getPlayerItems(userId);
+
+    const globalConfig = global.config;
+    // const userWeaponData = await getPlayerWeaponsData(username)
+
+    const skillpassdata = {
+      tier: season_passtier,
+      bucks: season_coins,
+      season_damage: bonusitem_damage,
+    };
+
+    const inventory = {
+      userId: userRow._id,
+      username: userRow.account.username,
+      lastnameupdate: userRow.account.nameupdate || 0,
+
+      items: userInventory,
+
+      coins: userRow.currency.coins,
+      boxes: userRow.currency.boxes,
+
+      sp: userRow.stats.sp,
+
+      seasondata: {
+        season_end: globalConfig.season_end,
+        skillpass: skillpassdata,
+      },
+
+      globalConfig: globalConfig,
+
+      weapons: userRow.inventory.weapons,
+      // weapondata: userWeaponData,
+      loadout: userRow.equipped.loadout,
+      last_collected: userRow.inventory.daily_reward.last_collected_at,
+      hat: userRow.equipped.hat,
+      top: userRow.equipped.top,
+      banner: userRow.equipped.banner,
+      pose: userRow.equipped.pose,
+      color: userRow.equipped.color,
+      hat_color: userRow.equipped.hat_color,
+      top_color: userRow.equipped.top_color,
+      banner_color: userRow.equipped.banner_color,
+      gadget: userRow.account.gadget,
+
+      boxrarities: rarityPercentages,
+      //  friends: userRow.social.friends || [],
+      // requests: userRow.social.requests || [],
+      serverlist,
+      nearestRegion: getServerByCountry(
+        userRow.account.country_code || "Unknown",
+      ),
+      weaponcatalog: WeaponsToBuy,
+      loadout_allowed_items: loadout_allowed_items,
+      in_app_purchases: RealMoneyPurchasesEnabled ? OFFERKEYS : "disabled",
+
+      server_timestamp: currentTimestampInGMT,
+    };
+
+    // Return the constructed object
+    return inventory;
+    //  })
+  } catch (error) {
+    // Catch and rethrow errors with additional context
+    throw new Error(`Failed to get user inventory: ${error.message}`);
+  }
 }
 
 module.exports = {
-    getUserInventory,
+  getUserInventory,
 };
